316e6fcd6570216fa326243aa690e8f684ab625c
Steve Moser <contact@stevemoser.org>
2015-11-25 20:35:36 -0500

    Add OSX SDK 10.6 (409.200) Docset changes

--- ImageBrowserController.m
+++ ImageBrowserController.m
@@ -1,60 +1,55 @@
 /*
-
-File: ImageBrowserController.m
-Abstract: IKImageBrowserView is a view that can display and browse a 
-	large amount of images and movies. This sample code demonstrate 
-	how to use it in a Cocoa Application.
-
-Version: 1.0
-
-Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple
-Computer, Inc. ("Apple") in consideration of your agreement to the
-following terms, and your use, installation, modification or
-redistribution of this Apple software constitutes acceptance of these
-terms.  If you do not agree with these terms, please do not use,
-install, modify or redistribute this Apple software.
-
-In consideration of your agreement to abide by the following terms, and
-subject to these terms, Apple grants you a personal, non-exclusive
-license, under Apple's copyrights in this original Apple software (the
-"Apple Software"), to use, reproduce, modify and redistribute the Apple
-Software, with or without modifications, in source and/or binary forms;
-provided that if you redistribute the Apple Software in its entirety and
-without modifications, you must retain this notice and the following
-text and disclaimers in all such redistributions of the Apple Software. 
-Neither the name, trademarks, service marks or logos of Apple Computer,
-Inc. may be used to endorse or promote products derived from the Apple
-Software without specific prior written permission from Apple.  Except
-as expressly stated in this notice, no other rights or licenses, express
-or implied, are granted by Apple herein, including but not limited to
-any patent rights that may be infringed by your derivative works or by
-other works in which the Apple Software may be incorporated.
-
-The Apple Software is provided by Apple on an "AS IS" basis.  APPLE
-MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
-THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
-FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND
-OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
-
-IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
-OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
-MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED
-AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
-STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-
-Copyright © 2006 Apple Computer, Inc., All Rights Reserved
-
-*/
-
+     File: ImageBrowserController.m 
+ Abstract: IKImageBrowserView is a view that can display and browse a large amount of images and movies.
+ This sample code demonstrate how to use it in a Cocoa Application.
+  
+  Version: 1.2 
+  
+ Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple 
+ Inc. ("Apple") in consideration of your agreement to the following 
+ terms, and your use, installation, modification or redistribution of 
+ this Apple software constitutes acceptance of these terms.  If you do 
+ not agree with these terms, please do not use, install, modify or 
+ redistribute this Apple software. 
+  
+ In consideration of your agreement to abide by the following terms, and 
+ subject to these terms, Apple grants you a personal, non-exclusive 
+ license, under Apple's copyrights in this original Apple software (the 
+ "Apple Software"), to use, reproduce, modify and redistribute the Apple 
+ Software, with or without modifications, in source and/or binary forms; 
+ provided that if you redistribute the Apple Software in its entirety and 
+ without modifications, you must retain this notice and the following 
+ text and disclaimers in all such redistributions of the Apple Software. 
+ Neither the name, trademarks, service marks or logos of Apple Inc. may 
+ be used to endorse or promote products derived from the Apple Software 
+ without specific prior written permission from Apple.  Except as 
+ expressly stated in this notice, no other rights or licenses, express or 
+ implied, are granted by Apple herein, including but not limited to any 
+ patent rights that may be infringed by your derivative works or by other 
+ works in which the Apple Software may be incorporated. 
+  
+ The Apple Software is provided by Apple on an "AS IS" basis.  APPLE 
+ MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION 
+ THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS 
+ FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND 
+ OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS. 
+  
+ IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL 
+ OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION, 
+ MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED 
+ AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), 
+ STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE 
+ POSSIBILITY OF SUCH DAMAGE. 
+  
+ Copyright (C) 2011 Apple Inc. All Rights Reserved. 
+  
+ */
 
 #import "ImageBrowserController.h"
 
-
-
-/* openFiles is a simple C function that open an NSOpenPanel and return an array of selected filepath */
+// openFiles is a simple C function that opens an NSOpenPanel and return an array of URLs
 static NSArray *openFiles()
 { 
     NSOpenPanel *panel;
@@ -63,33 +58,35 @@ static NSArray *openFiles()
     [panel setFloatingPanel:YES];
     [panel setCanChooseDirectories:YES];
     [panel setCanChooseFiles:YES];
-	int i = [panel runModalForTypes:nil];
-	if(i == NSOKButton){
-		return [panel filenames];
+	NSInteger i = [panel runModal];
+	if (i == NSOKButton)
+    {
+		return [panel URLs];
     }
     
     return nil;
-}    
-
+}
 
 /* Our datasource object */
-@interface myImageObject : NSObject{
+@interface myImageObject : NSObject
+{
     NSString *_path; 
 }
 @end
 
 @implementation myImageObject
 
-- (void) dealloc
+- (void)dealloc
 {
     [_path release];
     [super dealloc];
 }
 
 /* our datasource object is just a filepath representation */
-- (void) setPath:(NSString *) path
+- (void)setPath:(NSString *)path
 {
-    if(_path != path){
+    if(_path != path)
+    {
         [_path release];
         _path = [path retain];
     }
@@ -101,19 +98,19 @@ static NSArray *openFiles()
 #pragma mark item data source protocol
 
 /* let the image browser knows we use a path representation */
-- (NSString *)  imageRepresentationType
+- (NSString *)imageRepresentationType
 {
 	return IKImageBrowserPathRepresentationType;
 }
 
 /* give our representation to the image browser */
-- (id)  imageRepresentation
+- (id)imageRepresentation
 {
 	return _path;
 }
 
 /* use the absolute filepath as identifier */
-- (NSString *) imageUID
+- (NSString *)imageUID
 {
     return _path;
 }
@@ -121,21 +118,21 @@ static NSArray *openFiles()
 @end
 
 
-
 /* the controller */
 @implementation ImageBrowserController
 
-- (void) dealloc
+- (void)dealloc
 {
     [_images release];
     [_importedImages release];
     [super dealloc];
 }
 
-- (void) awakeFromNib
+- (void)awakeFromNib
 {
-	/* create two arrays : the first one is our datasource representation, the second one are temporary imported images (for thread safeness ) 
-	*/
+    // create two arrays : the first one is our datasource representation,
+    // the second one are temporary imported images (for thread safeness) 
+
     _images = [[NSMutableArray alloc] init];
     _importedImages = [[NSMutableArray alloc] init];
     
@@ -146,7 +143,7 @@ static NSArray *openFiles()
 }
 
 /* entry point for reloading image-browser's data and setNeedsDisplay */
-- (void) updateDatasource
+- (void)updateDatasource
 {
     //-- update our datasource, add recently imported items
     [_images addObjectsFromArray:_importedImages];
@@ -158,15 +155,15 @@ static NSArray *openFiles()
     [_imageBrowser reloadData];
 }
 
+
 #pragma mark -
 #pragma mark import images from file system
 
-/* code that parse a repository and add all items in an independant array,
+/* Code that parse a repository and add all items in an independant array,
    When done, call updateDatasource, add these items to our datasource array
    This code is performed in an independant thread.
 */
-
-- (void) addAnImageWithPath:(NSString *) path
+- (void)addAnImageWithPath:(NSString *)path
 {   
     myImageObject *p;
 
@@ -177,70 +174,77 @@ static NSArray *openFiles()
     [p release];
 }
 
-- (void) addImagesWithPath:(NSString *) path recursive:(BOOL) recursive
+- (void)addImagesWithPath:(NSString *)path recursive:(BOOL)recursive
 {
-    int i, n;
+    NSInteger i, n;
     BOOL dir;
 
     [[NSFileManager defaultManager] fileExistsAtPath:path isDirectory:&dir];
     
-    if(dir){
-        NSArray *content = [[NSFileManager defaultManager] directoryContentsAtPath:path];
+    if (dir)
+    {
+        NSArray *content = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:path error:nil];
         
         n = [content count];
 
-		/* parse the directory content*/
-        for(i=0; i<n; i++){
-            if(recursive)
+		// parse the directory content
+        for (i=0; i<n; i++)
+        {
+            if (recursive)
                 [self addImagesWithPath:[path stringByAppendingPathComponent:[content objectAtIndex:i]] recursive:YES];
             else
                 [self addAnImageWithPath:[path stringByAppendingPathComponent:[content objectAtIndex:i]]];
         }
     }
     else
+    {
         [self addAnImageWithPath:path];
+    }
 }
 
 /* performed in an independant thread, parse all paths in "paths" and add these paths in our temporary array */
-- (void) addImagesWithPaths:(NSArray *) paths
+- (void)addImagesWithPaths:(NSArray *)urls
 {   
-    int i, n;
+    NSInteger i, n;
     
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-    [paths  retain];
+    [urls retain];
 
-    n = [paths count];
-    for(i=0; i<n; i++){
-        NSString *path = [paths objectAtIndex:i];
-        [self addImagesWithPath:path recursive:NO];
+    n = [urls count];
+    for ( i= 0; i < n; i++)
+    {
+        NSURL *url = [urls objectAtIndex:i];
+        [self addImagesWithPath:[url path] recursive:NO];
     }
 
 	/* update the datasource in the main thread */
     [self performSelectorOnMainThread:@selector(updateDatasource) withObject:nil waitUntilDone:YES];
 
-    [paths release];
+    [urls release];
     [pool release];
 }
+
 
 #pragma mark -
 #pragma mark actions
 
 /* "add" button was clicked */
-- (IBAction) addImageButtonClicked:(id) sender
+- (IBAction)addImageButtonClicked:(id)sender
 {   
-    NSArray *path = openFiles();
+    NSArray *urls = openFiles();
     
-    if(!path){ 
-        NSLog(@"No path selected, return..."); 
+    if (!urls)
+    { 
+        NSLog(@"No files selected, return..."); 
         return; 
     }
 	
 	/* launch import in an independent thread */
-    [NSThread detachNewThreadSelector:@selector(addImagesWithPaths:) toTarget:self withObject:path];
+    [NSThread detachNewThreadSelector:@selector(addImagesWithPaths:) toTarget:self withObject:urls];
 }
 
 /* action called when the zoom slider did change */
-- (IBAction) zoomSliderDidChange:(id)sender
+- (IBAction)zoomSliderDidChange:(id)sender
 {
 	/* update the zoom value to scale images */
     [_imageBrowser setZoomValue:[sender floatValue]];
@@ -249,6 +253,7 @@ static NSArray *openFiles()
     [_imageBrowser setNeedsDisplay:YES];
 }
 
+
 #pragma mark -
 #pragma mark IKImageBrowserDataSource
 
@@ -256,17 +261,16 @@ static NSArray *openFiles()
    Our datasource representation is a simple mutable array
 */
 
-- (int) numberOfItemsInImageBrowser:(IKImageBrowserView *) view
+- (NSUInteger)numberOfItemsInImageBrowser:(IKImageBrowserView *)view
 {
 	/* item count to display is our datasource item count */
     return [_images count];
 }
 
-- (id) imageBrowser:(IKImageBrowserView *) view itemAtIndex:(int) index
+- (id)imageBrowser:(IKImageBrowserView *)view itemAtIndex:(NSUInteger)index
 {
     return [_images objectAtIndex:index];
 }
-
 
 
 /* implement some optional methods of the image-browser's datasource protocol to be able to remove and reoder items */
@@ -274,23 +278,23 @@ static NSArray *openFiles()
 /*	remove
 	The user wants to delete images, so remove these entries from our datasource.	
 */
-- (void) imageBrowser:(IKImageBrowserView *) view removeItemsAtIndexes: (NSIndexSet *) indexes
+- (void)imageBrowser:(IKImageBrowserView *)view removeItemsAtIndexes:(NSIndexSet *)indexes
 {
 	[_images removeObjectsAtIndexes:indexes];
 }
 
-/* reordering 
-	The user wants to reorder images, update our datasource and the browser will reflect our changes
-*/
-- (BOOL) imageBrowser:(IKImageBrowserView *) view  moveItemsAtIndexes: (NSIndexSet *)indexes toIndex:(unsigned int)destinationIndex
+// reordering:
+// The user wants to reorder images, update our datasource and the browser will reflect our changes
+- (BOOL)imageBrowser:(IKImageBrowserView *)view moveItemsAtIndexes:(NSIndexSet *)indexes toIndex:(NSUInteger)destinationIndex
 {
-      int index;
+      NSUInteger index;
       NSMutableArray *temporaryArray;
 	  
       temporaryArray = [[[NSMutableArray alloc] init] autorelease];
       
 	  /* first remove items from the datasource and keep them in a temporary array */
-      for(index=[indexes lastIndex]; index != NSNotFound; index = [indexes indexLessThanIndex:index]){
+      for (index = [indexes lastIndex]; index != NSNotFound; index = [indexes indexLessThanIndex:index])
+      {
           if (index < destinationIndex)
               destinationIndex --;
           
@@ -300,13 +304,15 @@ static NSArray *openFiles()
       }
   
 	  /* then insert removed items at the good location */
-      int n = [temporaryArray count];
-      for(index=0; index < n; index++){
+      NSInteger n = [temporaryArray count];
+      for (index=0; index < n; index++)
+      {
           [_images insertObject:[temporaryArray objectAtIndex:index] atIndex:destinationIndex];
       }
 	
       return YES;
 }
+
 
 #pragma mark -
 #pragma mark drag n drop 
@@ -322,7 +328,7 @@ static NSArray *openFiles()
     return NSDragOperationCopy;
 }
 
-- (BOOL) performDragOperation:(id <NSDraggingInfo>)sender
+- (BOOL)performDragOperation:(id <NSDraggingInfo>)sender
 {
     NSData *data = nil;
     NSString *errorDescription;
@@ -333,7 +339,8 @@ static NSArray *openFiles()
     if ([[pasteboard types] containsObject:NSFilenamesPboardType]) 
         data = [pasteboard dataForType:NSFilenamesPboardType];
 
-    if(data){
+    if (data)
+    {
 		/* retrieves paths */
         NSArray *filenames = [NSPropertyListSerialization propertyListFromData:data 
                                              mutabilityOption:kCFPropertyListImmutable 
@@ -342,9 +349,9 @@ static NSArray *openFiles()
     
     
 		/* add paths to our datasource */
-        int i;
-        int n = [filenames count];
-        for(i=0; i<n; i++){
+        NSInteger i;
+        NSInteger n = [filenames count];
+        for (i=0; i<n; i++){
             [self addAnImageWithPath:[filenames objectAtIndex:i]];
         }
 		
